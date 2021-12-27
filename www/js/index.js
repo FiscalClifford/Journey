12f6376e410f3f63d36657f660659cad3f172e0a
Brennan Giles <collegegiles@gmail.com>
2021-12-26 21:33:40 -0800

    Better roads
    now using shuffle()!

--- www/js/index.js
+++ www/js/index.js
@@ -28,6 +28,24 @@ function getRand(min, max) {
     return matrix;
   }
 
+  function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
 ///////////////////////////////////////////////////////////////////// GLOBALS ////////////////////////////////////////////////////////
 
 var sizeX = 10;
@@ -523,6 +541,7 @@ function findPath(startPoint, endPoint){
 
     //main loop
     while (queue.length) {
+        queue = shuffle(queue);
         var currentLocation = queue.shift();
         if (currentLocation.x == endPoint.x && currentLocation.y == endPoint.y){
             final = printPath(currentLocation);
